8ec40b3d14b8cb37284da02db6a9a6cb0acc9bd9
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-16 05:56:46 +0000

    4° commit semaforo

--- semaforo_hooks_react/src/Home.js
+++ semaforo_hooks_react/src/Home.js
@@ -3,10 +3,22 @@ import React from 'react';
 const Home=()=>{
    return(
     <>
-    <div className='container-fluid'>
-    <div className='titulo'>Hola Mundo</div>
-    <div className='icono_1 btn btn-warning'>
-        <i className="fa-solid fa-clock"></i>
+    <div id="Trafico-luces">
+    <div className='container brazo_semaforo'>
+        <div className='container'>
+            <div className='row'>
+                    <div className='col-12 col-md-12 col-sm-12 red_ligth'>
+                    <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0"></i></div>
+            </div>
+                <div className='row'>
+                     <div className='col-12 col-md-12 col-sm-12 amarilla_ligth'>
+                     <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0"></i></div>
+                </div>
+                    <div className='row'>
+                        <div className='col-12 col-md-12 col-sm-12 verde_ligth'>
+                        <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0"></i></div>
+                    </div>
+        </div>
     </div>
     </div>
     </>
