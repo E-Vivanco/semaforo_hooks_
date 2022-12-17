1e447b26d44d58514b3fb2f7108f1420d200c477
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-17 01:37:17 +0000

    seleccion de botones generada

--- semaforo_hooks_react/src/App.js
+++ semaforo_hooks_react/src/App.js
@@ -1,13 +1,14 @@
-//import './useState';
+import React from 'react'
 import Home from './Home';
 import './styles.css'
-
+    
 
 const App=()=>{
-
+   
     return(
-        
+       <> 
        <Home /> 
+       </>
     )
 }
 
--- semaforo_hooks_react/src/Home.js
+++ semaforo_hooks_react/src/Home.js
@@ -1,57 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 
-const Home=(props)=>{
-    const [color_R , setColor1] =useState("red")
-    const [show_R,setShow_R]=useState(true)
-    const [color_A , setColor2] =useState("yellow")
-    const [color_V , setColor3] =useState("green")
-    
-    useEffect(()=>{
-        setColor1("#de5d5b")
-    },[color_R])
-
-    useEffect(()=>{
-        setColor2("#eef800")
-    },[color_A])
-
-    useEffect(()=>{
-        setColor3("#00f800")
-    },[color_V])
-
-
+const Home=()=>{
+    const [color, setColor] =useState(" ")
     return(
     <>
-    <div id="Trafico-luces">
-    <div className='container brazo_semaforo'>
-        <div className='container'>
+    <div id="Trafico_luces">
+    <div className='brazo_semaforo'>
+        <div className='semaforo'>
             <div className='row'>
                     <div className='col-12 col-md-12 col-sm-12 red_ligth'>
-                    <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0" 
-                    onclick={()=>{
-                        setShow_R(show_R)
-                    }}></i>
-                        {color_R}
+                      <button className={'btn btn-outline-danger'+(color === 'red'? "btn btn-outline-light": null)} onClick={()=>setColor('red')}>                        
+                        <i className="fa-solid fa-circle mx-2" ></i></button>
+                       
+                    </div>
+                     <div className='col-12 col-md-12 col-sm-12 amarilla_ligth'>
+                     <button className={'btn btn-outline-yellow '+(color === 'yellow'? "btn btn-outline-light": null)} onClick={()=>setColor('yellow')}>
+                     <i className="fa-regular fa-circle mx-2"></i></button>
+                        
+                     </div>
+                    <div className='col-12 col-md-12 col-sm-12 verde_ligth'>
+                    <button className={'btn btn-outline-success'+(color === 'green'? "btn btn-outline-light": null)} onClick={()=>setColor('green')}>
+                    <i className="fa-regular fa-circle mx-2"></i></button>
                     </div>
             </div>
-                <div className='row'>
-                     <div className='col-12 col-md-12 col-sm-12 amarilla_ligth'>
-                     <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0"></i>
-                     {color_A}
-                     </div>
-                </div>
-                    <div className='row'>
-                        <div className='col-12 col-md-12 col-sm-12 verde_ligth'>
-                        <i class="fa-regular fa-circle fa-3x my-1 mx-0 px-0"></i>
-                        {color_V}
-                        </div>
-                    </div>
         </div>
     </div>
     </div>
     </>
    )
-}
 
+    }
 
 export default Home
